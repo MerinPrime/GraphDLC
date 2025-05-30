bf88bbb73b04e8ed33117117deb811159ccef049
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-05-31 01:47:00 +0300

    Added patch loader

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { ModLoaderV2 } from "./modloader_v2";
+import {PatchLoader} from "./loader/patchloader";
 
-ModLoaderV2.addDefinitionPatch("ArrowShader", function (name: string, module: any): any {
-    ModLoaderV2.setDefinition("ArrowShader", class ArrowShaderRGB extends module {
+const patchLoader = new PatchLoader();
+patchLoader.hook();
+patchLoader.addDefinitionPatch("ArrowShader", function (name: string, module: any): any {
+    patchLoader.setDefinition("ArrowShader", class ArrowShaderRGB extends module {
         makeFragmentShader(): string {
             let shaderCode: string = super.makeFragmentShader();
-            
+
             shaderCode = shaderCode.replace(
                 "if (u_signal == 5) signalColor = vec4(1.0, 0.8, 0.2, 1.0);",
                 "if (u_signal == 5) signalColor = vec4(0.0, 0.8, 0.0, 1.0);"
             );
-            
+
             return shaderCode;
         }
     });
---
+++ src/loader/patchloader.ts
@@ -0,0 +1,95 @@
+export class PatchLoader {
+    private definitions: Record<string, any> = {};
+    private instances: Record<string, any> = {};
+    private patches: Array<(name: string, definition: any) => boolean> = [];
+    private originalCall?: (this: Function, thisArg: any, ...argArray: any[]) => any;
+    
+    constructor() {
+        
+    }
+    
+    hook() {
+        if (this.originalCall)
+            return;
+
+        this.originalCall = Function.prototype.call;
+
+        const modLoader = this;
+        Function.prototype.call = function(thisArg: any, ...args: any[]) {
+            return modLoader.patchedCall(this, thisArg, ...args);
+        };
+    }
+
+    private patchedCall(originalFunction: Function, thisArg: any, ...argArray: any[]): any {
+        if (!this.originalCall)
+            throw new Error("PatchLoader is not hooked");
+
+        const result = Reflect.apply(originalFunction, thisArg, argArray);
+        const exports = argArray[1] as Record<string, any> | undefined;
+
+        if (!exports || exports.__esModule !== true) return result;
+
+        for (const key of Object.keys(exports)) {
+            if (key === '__esModule') continue;
+
+            let definition = exports[key];
+
+            if (typeof definition === 'function' && /^class\s/.test(Function.prototype.toString.call(definition))) {
+                const modLoader = this;
+                const original = definition;
+                definition = class extends original {
+                    constructor(...ctorArgs: any[]) {
+                        super(...ctorArgs);
+                        modLoader.setInstance(key, this);
+                    }
+                };
+            }
+
+            this.setDefinition(key, definition);
+            exports[key] = this.definitions[key];
+        }
+
+        return result;
+    }
+
+    public getDefinition(name: string): any {
+        return this.definitions[name];
+    }
+
+    public setDefinition(name: string, definition: any): void {
+        this.definitions[name] = definition;
+
+        const oldPatches = this.patches;
+        const newPatches: typeof this.patches = [];
+        this.patches = [];
+        
+        for (const patch of oldPatches) {
+            const removePatch = patch(name, definition);
+            if (!removePatch)
+                newPatches.push(patch);
+        }
+
+        this.patches = newPatches;
+    }
+
+    public getInstance(name: string): any {
+        return this.instances[name];
+    }
+
+    public setInstance(name: string, instance: any) {
+        this.instances[name] = instance;
+    }
+
+    public addManualPatch(patch: (name: string, definition: any) => boolean): void {
+        this.patches.push(patch);
+    }
+
+    public addDefinitionPatch(target: string, patch: (name: string, definition: any) => void): void {
+        this.addManualPatch((name, definition) => {
+            if (name !== target)
+                return false;
+            patch(name, definition);
+            return true;
+        });
+    }
+}
