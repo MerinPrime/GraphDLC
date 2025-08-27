3868cae6d9ed1deb193b6c27224d686c79e7797b
FotisDa <96889060+MerinPrime@users.noreply.github.com>
2025-08-27 11:21:26 +0300

    Fix branch debug

--- src/ast/branch/branchOptimizer.ts
+++ src/ast/branch/branchOptimizer.ts
@@ -48,7 +48,7 @@ export class BranchOptimizer {
                         }
                         if (isValid) {
                             const newNode = new ASTNode().combine(validNodes);
-                            // newNode.isBranch = true;
+                            newNode.isBranch = true;
                             node.allEdges.push(newNode);
                             node.edges.push(newNode);
                             node.filterDuplicates();
