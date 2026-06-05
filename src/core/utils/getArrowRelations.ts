6db7328ad4e35ea56bdc6ba69c762a8724532115
MerinPrime <96889060+MerinPrime@users.noreply.github.com>
2026-06-06 00:40:22 +0300

    perf: optimize relation lookup in getArrowRelations

--- src/core/utils/getArrowRelations.ts
+++ src/core/utils/getArrowRelations.ts
@@ -1,67 +1,73 @@
 import { ArrowType } from './ArrowType';
 
-export function getArrowRelations(type: ArrowType): Array<[number, number]> {
-    switch (type) {
-        case ArrowType.ARROW:
-        case ArrowType.BLOCKER:
-        case ArrowType.DELAY:
-        case ArrowType.DETECTOR:
-        case ArrowType.LOGIC_NOT:
-        case ArrowType.LOGIC_AND:
-        case ArrowType.LOGIC_XOR:
-        case ArrowType.LATCH:
-        case ArrowType.FLIP_FLOP:
-        case ArrowType.RANDOM:
-        case ArrowType.LEVEL_SOURCE:
-        case ArrowType.DIRECTIONAL_BUTTON:
-            return [[-1, 0]];
-        case ArrowType.SOURCE:
-        case ArrowType.IMPULSE:
-        case ArrowType.BUTTON:
-            return [
-                [-1, 0],
-                [1, 0],
-                [0, -1],
-                [0, 1],
-            ];
-        case ArrowType.SPLITTER_UP_DOWN:
-            return [
-                [-1, 0],
-                [1, 0],
-            ];
-        case ArrowType.SPLITTER_UP_RIGHT:
-            return [
-                [-1, 0],
-                [0, 1],
-            ];
-        case ArrowType.SPLITTER_UP_RIGHT_LEFT:
-            return [
-                [0, -1],
-                [-1, 0],
-                [0, 1],
-            ];
-        case ArrowType.BLUE_ARROW:
-            return [[-2, 0]];
-        case ArrowType.DIAGONAL_ARROW:
-            return [[-1, 1]];
-        case ArrowType.SPLITTER_UP_UP:
-            return [
-                [-1, 0],
-                [-2, 0],
-            ];
-        case ArrowType.SPLITTER_RIGHT_UP:
-            return [
-                [0, 1],
-                [-2, 0],
-            ];
-        case ArrowType.SPLITTER_UP_DIAGONAL:
-            return [
-                [-1, 0],
-                [-1, 1],
-            ];
-        case ArrowType.EMPTY:
-        case ArrowType.LEVEL_TARGET:
-        default:
-            return [];
-    }
+type RelationCoords = Array<[number, number]>;
+
+const ARROW_RELATIONS_MAP: Record<ArrowType, RelationCoords> = {
+    [ArrowType.ARROW]: [[-1, 0]],
+    [ArrowType.BLOCKER]: [[-1, 0]],
+    [ArrowType.DELAY]: [[-1, 0]],
+    [ArrowType.DETECTOR]: [[-1, 0]],
+    [ArrowType.LOGIC_NOT]: [[-1, 0]],
+    [ArrowType.LOGIC_AND]: [[-1, 0]],
+    [ArrowType.LOGIC_XOR]: [[-1, 0]],
+    [ArrowType.LATCH]: [[-1, 0]],
+    [ArrowType.FLIP_FLOP]: [[-1, 0]],
+    [ArrowType.RANDOM]: [[-1, 0]],
+    [ArrowType.LEVEL_SOURCE]: [[-1, 0]],
+    [ArrowType.DIRECTIONAL_BUTTON]: [[-1, 0]],
+
+    [ArrowType.SOURCE]: [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1],
+    ],
+    [ArrowType.IMPULSE]: [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1],
+    ],
+    [ArrowType.BUTTON]: [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1],
+    ],
+
+    [ArrowType.SPLITTER_UP_DOWN]: [
+        [-1, 0],
+        [1, 0],
+    ],
+    [ArrowType.SPLITTER_UP_RIGHT]: [
+        [-1, 0],
+        [0, 1],
+    ],
+    [ArrowType.SPLITTER_UP_RIGHT_LEFT]: [
+        [0, -1],
+        [-1, 0],
+        [0, 1],
+    ],
+    [ArrowType.BLUE_ARROW]: [[-2, 0]],
+    [ArrowType.DIAGONAL_ARROW]: [[-1, 1]],
+    [ArrowType.SPLITTER_UP_UP]: [
+        [-1, 0],
+        [-2, 0],
+    ],
+    [ArrowType.SPLITTER_RIGHT_UP]: [
+        [0, 1],
+        [-2, 0],
+    ],
+    [ArrowType.SPLITTER_UP_DIAGONAL]: [
+        [-1, 0],
+        [-1, 1],
+    ],
+
+    [ArrowType.EMPTY]: [],
+    [ArrowType.WALL]: [],
+    [ArrowType.LEVEL_TARGET]: [],
+};
+
+export function getArrowRelations(type: ArrowType): RelationCoords {
+    return ARROW_RELATIONS_MAP[type] ?? [];
 }
