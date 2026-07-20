a6f4d8555e7b08b4a37141ada78202e0d8c2c7b9
MerinPrime <96889060+MerinPrime@users.noreply.github.com>
2026-07-20 23:56:39 +0300

    refactor: graphdlc versions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { UpdateManager } from './core/credentials/UpdateManager';
-import { GraphDLC } from './core/GraphDLC';
-import { PatchLoader } from './core/PatchLoader';
-import { InjectGraphDLCv2 } from './graphdlcv2';
+import { InjectGraphDLCv2 } from './versions/graphdlcv2';
+import { InjectGraphDLCv3 } from './versions/graphdlcv3';
 
 const selectedBundle = localStorage.getItem('arrows:selectedBundleId');
 const selectedVersion = selectedBundle ?? '1_4';
@@ -10,17 +9,10 @@ if (selectedBundle === null)
 
 if (selectedVersion === '1_2_1') {
     localStorage.removeItem('graphdlc:unsupported');
-
     InjectGraphDLCv2();
 } else if (selectedVersion === '1_4') {
     localStorage.removeItem('graphdlc:unsupported');
-
-    const patchLoader = new PatchLoader();
-    patchLoader.hook();
-    const graphDLC = new GraphDLC(patchLoader);
-    graphDLC.setup();
-
-    window.graphdlc = graphDLC;
+    InjectGraphDLCv3();
 } else if (localStorage.getItem('graphdlc:unsupported') !== '1') {
     localStorage.setItem('graphdlc:unsupported', '1');
     const updateManager = new UpdateManager();
