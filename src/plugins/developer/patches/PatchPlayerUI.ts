e809556f81b7b4ffeb4a1e1b80341e5769f0237c
MerinPrime <96889060+MerinPrime@users.noreply.github.com>
2026-08-31 23:28:49 +0300

    chore: add some debug info

--- src/plugins/developer/patches/PatchPlayerUI.ts
+++ src/plugins/developer/patches/PatchPlayerUI.ts
@@ -66,6 +66,15 @@ export const PatchPlayerUI: IPatcher = (
                             debugLines.push(
                                 `IsBreakpoint: ${nodeState.isBreakpoint}`,
                             );
+                            debugLines.push(
+                                `Links: ${node.linkCounts.reduce((sum, count) => sum + count, 0)}`,
+                            );
+                            debugLines.push(
+                                `Backlinks: ${node.backLinkCounts.reduce((sum, count) => sum + count, 0)}`,
+                            );
+                            debugLines.push(
+                                `HasDetector: ${node.detectedLink !== null}`,
+                            );
                         }
                     } else {
                         debugLines.push('Not Raw Engine');
